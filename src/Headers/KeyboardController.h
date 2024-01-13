1ffa98d47cf87e8af131c91deb4e105d500a0c95
juhotuominen <juhonen00@gmail.com>
2024-01-13 02:18:16 +0200

    modify moving to smoother

--- src/Headers/KeyboardController.h
+++ src/Headers/KeyboardController.h
@@ -14,50 +14,27 @@ public:
 		transform = &entity->getComponent<TransformComponent>();
 	}
 
-	void update() override
-	{
-		if (Game::event.type == SDL_KEYDOWN)
-		{
-			switch (Game::event.key.keysym.sym)
-			{
-			case SDLK_w:
-				transform->velocity.y = -1;
-				break;
-			case SDLK_s:
-				transform->velocity.y = 1;
-				break;
-			case SDLK_a:
-				transform->velocity.x = -1;
-				break;
-			case SDLK_d:
-				transform->velocity.x = 1;
-				break;
+	const Uint8* keystates = SDL_GetKeyboardState(NULL);
 
-			default:
-				break;
-			}
+	void update() override {
+
+		transform->velocity.x = 0;
+		transform->velocity.y = 0;
+
+		if (keystates[SDL_SCANCODE_W]) {
+			transform->velocity.y = -1;
 		}
 
-		if (Game::event.type == SDL_KEYUP)
-		{
-			switch (Game::event.key.keysym.sym)
-			{
-			case SDLK_w:
-				transform->velocity.y = 0;
-				break;
-			case SDLK_s:
-				transform->velocity.y = 0;
-				break;
-			case SDLK_a:
-				transform->velocity.x = 0;
-				break;
-			case SDLK_d:
-				transform->velocity.x = 0;
-				break;
+		if (keystates[SDL_SCANCODE_A]) {
+			transform->velocity.x = -1;
+		}
 
-			default:
-				break;
-			}
+		if (keystates[SDL_SCANCODE_S]) {
+			transform->velocity.y = 1;
+		}
+
+		if (keystates[SDL_SCANCODE_D]) {
+			transform->velocity.x = 1;
 		}
 	}
 };
